f3833af6dc6ea6522831a7653dd81d91d4849f10
ankit4a4 <137420092+ankit4a4@users.noreply.github.com>
2024-06-07 12:38:47 +0530

    frist

---
+++ src/components/Todo.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addTodo, removeTodo } from '../store/feature/todoSlice';
+import '../components/Todo.css'
+import {  toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function Todo() {
+  const dispatch = useDispatch();
+  const [input, setInput] = useState("")
+  const handlesubmit = (e) => {
+    e.preventDefault();
+   if(input === ""){
+    toast.error("please enter a value")
+   }else{
+    dispatch(addTodo(input))
+    setInput("")
+    toast.success("Successful...")
+   }
+  }
+  const todos = useSelector((state => state.todos.todo))
+  const handleremoveTodo = (id) => {
+    dispatch(removeTodo(id))
+    toast.warning("This item deleted...")
+  }
+  return (
+    <>
+      <form onSubmit={handlesubmit}>
+        <input type="text" onChange={(e) => setInput(e.target.value)} value={input} />
+        <button type='submit'>Add</button>
+        <ul>
+        {todos.map((item) => (
+          <li >{item.text} <p onClick={() => handleremoveTodo(item.id)} style={{ cursor: "pointer" , float:"right"}} >💩</p></li>
+        ))}
+      </ul>
+      </form>
+      
+    </>
+  )
+}
+
+export default Todo
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import { ToastContainer } from "react-toastify";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+    <ToastContainer
+      position="top-right"
+      autoClose={1000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
+  </Provider>
+);
+reportWebVitals();
